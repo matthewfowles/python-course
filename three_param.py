6953db23e60b34c9aec4f2a68f61aee726ca6ad4
Matthew Fowles <matthew.fowles@cohaesus.co.uk>
2015-01-03 20:37:35 +0000

    objective 11

---
+++ three_param.py
@@ -0,0 +1,13 @@
+
+
+def my_func(a, b = 'b was not entered', c = 'c was not entered') :
+	print a
+	print b
+	print c
+
+
+my_func('This is first param')
+my_func('This is first param', 'This is second param')
+my_func('This is first param', 'This is second param', 'This is third param')
+
+print my_func
