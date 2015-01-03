d9304d31a476b168d1ef9bcbba90baafb73ea19b
Matthew Fowles <matthew.fowles@cohaesus.co.uk>
2015-01-03 20:49:11 +0000

    Objective 12

---
+++ guessing_game.py
@@ -0,0 +1,35 @@
+#
+# This is a guessing number game.
+#
+"""
+This program takes makes a random number between 1 and 99 and asks the user to guess it. 
+"""
+
+from random import randint
+
+
+number = randint(1,99)
+
+# create main function
+def main() :
+
+	print 'Try to guess the number between 0 and 99.'
+
+
+	while True:
+
+
+		guess = int(raw_input("Please enter a guess: "))
+
+
+		if guess > number :
+			print 'Too high! Try again.'
+			continue
+		elif guess < number :
+			print 'Too low! Try again.'
+			continue
+		elif guess == number :
+			print 'Congratulations you got it!'
+			break
+
+main()
